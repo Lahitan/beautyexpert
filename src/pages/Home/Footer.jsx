ad1aa46c20f5fe795d8c6486f985a7df14cf2200
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-03-29 22:55:16 +0100

    feat:add the all right reserved sections

--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -7,19 +7,21 @@ import linkedin from "../../assets/linkedin-logo.png"
 
 const Footer = () => {
 	return (
-		<div className=" flex bg-secondary-brand text-white text-center md:text-left">
+
+		// footer section
+		<div className=" flex bg-secondary-brand text-white text-center md:text-left font-openSans">
 			<div className=" w-[70%] mx-auto my-10 grid-1 gap-8 grid md:grid-cols-2 lg:grid-cols-4  ">
-	
-						<div className="about-us ">
+	{/* about us section */}
+						<div >
 							<h4 className="text-xl font-bold " >ABOUT US</h4>
 							<p className=" my-4 text-xs ">
-							At Beauty Xpats, we bridge the gap between beauty and convenience. <br />
-							We're passionate about empowering beauty professionals and simplifying the beauty experience for clients. Beauty Xpats is a one-stop platform connecting talented beauty experts with clients seeking exceptional service.
+								At Beauty Xpats, we bridge the gap between beauty and convenience. <br />
+								We're passionate about empowering beauty professionals and simplifying the beauty experience for clients. Beauty Xpats is a one-stop platform connecting talented beauty experts with clients seeking exceptional service.
 							</p>
 							<NavLink className=" text-xs " to="/"  >contact@beautyxpats.org</NavLink>
 						</div>
-
-						<div className=" navigation ">
+{/* navigation section */}
+						<div >
 							<h4 className="text-xl font-bold mb-4  " >NAVIGATION</h4>
 							<div className="links">
 								<NavLink to="/"  className=" my-4 ">Home</NavLink><br />
@@ -29,23 +31,54 @@ const Footer = () => {
 								<NavLink to="/">About us</NavLink>
 							</div>
 						</div>
-						<div className="legal">
+{/* legal section		 */}
+						<div >
 							<h4 className="text-xl font-bold mb-4 " >LEGAL</h4>
-							<div className="links">
+							<div >
 								<NavLink to="/" >Terms and Conditions</NavLink><br />
 								<NavLink to="/">Privacy Policy</NavLink>
 							</div>	
 						</div>
-						<div className="help">
+{/* help section */}
+						<div >
 							<h4 className="text-xl font-bold mb-4" >HELP</h4>
 							<NavLink to="/">Contact us</NavLink>
 						</div>
-
-						
-					</div>
-				</div>
+{/* Follow us section */}
+						<div className="flex  ">
+							<p className=" text-base font-bold mr-5" >
+								Follow us: 
+							</p>
+{/*social media icons  */}
+							<div className="flex justify-between">
+								<div>
+									<img src={fb} alt="facebook link" className=" mx-2 "/>
+								</div>
+								<div>
+									<img src={ig} alt="instagram link" className=" mx-2 "/>
+								</div>
+								<div>
+									<img src={twitter} alt="twitter link" className="mx-2 "/>
+								</div>
+								<div>
+									<img src={linkedin} alt="linkedin link" className="mx-2 "/>
+								</div>
+							</div>	
+						</div>
+{/* copyright and all rights reserved */}
+						<div className="text-xs mt-20">
+							<span>© 2024 Beauty Xpats: </span>
+							<a
+							href="/"
+							>All Rights Reserved</a>
+						</div>
+			</div>
+		</div>
 		
 	);
 };
 
+
 export default Footer;
+
+
