4ee2794802b3bee904645071c4f70eee741f522d
Tamarapreye-M <preyemichael116@gmail.com>
2024-03-29 11:18:52 +0100

    set up

---
+++ src/pages/Home/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+	return (
+		<div className="bg-primary-brand">
+			<div className="w-[90%] mx-auto"></div>
+		</div>
+	);
+};
+
+export default Main;
