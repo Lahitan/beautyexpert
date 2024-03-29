45d41695bee6531ef12562cf07a332ff93203fc7
Tamarapreye-M <preyemichael116@gmail.com>
2024-03-29 11:32:26 +0100

    i added a sample

--- src/pages/Home/Main.jsx
+++ src/pages/Home/Main.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Main = () => {
 	return (
 		<div className="bg-primary-brand">
-			<div className="w-[90%] mx-auto"></div>
+			<div className="w-[90%] mx-auto">
+				<p>This is Tamara's work</p>
+			</div>
 		</div>
 	);
 };
