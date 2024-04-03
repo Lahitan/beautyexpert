2b8ee9c90bbec1cda2c1735f0223b3ff1ac4d511
Tamarapreye-M <preyemichael116@gmail.com>
2024-04-03 08:16:19 +0100

    Log in navigation configured

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import ErrorPage from "./pages/ErrorPage";
+import About from "./pages/About.jsx";
+import LoginRoot from "./pages/Login/LoginRoot.jsx";
+import CustomerCreateAcount from "./pages/Login/CustomerCreateAcount.jsx";
+import Login from "./pages/Login/Login.jsx";
+import ForgetPassword from "./pages/Login/ForgetPassword.jsx";
 
 const router = createBrowserRouter([
 	{
@@ -11,6 +16,27 @@ const router = createBrowserRouter([
 		element: <App />,
 		errorElement: <ErrorPage />,
 		children: [],
+	},
+	{
+		path: "/about",
+		element: <About />,
+	},
+	{
+		path: "/loginRoot",
+		element: <LoginRoot />,
+	},
+	{
+		path: "/customerCreateAccount",
+		element: <CustomerCreateAcount />,
+	},
+	{
+		path: "/login",
+		element: <Login />,
+		children: [],
+	},
+	{
+		path: "forgetPassword",
+		element: <ForgetPassword />,
 	},
 ]);
 
--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -69,6 +69,7 @@ const Header = () => {
 							</li>
 							<li>
 								<NavLink
+									to={"/about"}
 									className={({ isActive }) =>
 										isActive ? " text-[#3992EE]" : "text-[#B1B1B1]"
 									}
@@ -81,6 +82,7 @@ const Header = () => {
 					{/* adjusted the padding in the button for responsiveness*/}
 					<div className="">
 						<NavLink
+							to={"loginRoot"}
 							className={
 								" border border-tertiary-brand  me-1 lg:me-3 font-semibold rounded-full py-1 px-3 lg:py-2 lg:px-5"
 							}
