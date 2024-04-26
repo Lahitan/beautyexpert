7e71af9833e8bf532f8f7dec80acbc1c048bfc0f
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-04-26 20:34:44 +0100

    feat:add CustomerServices and CustomerServicesCar

---
+++ src/pages/CustomerPages/CustomerServicesCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CustomerServicesCard = ({ image, title, stars, price, heartLogo }) => {
+	return (
+		<div className="shadow-special rounded-xl border border-[#3992ED] p-2 flex flex-col justify-between ">
+			<img src={image} alt="" className=" w-full" />
+			<h5 className="font-semibold text-lg lg:text-2xl my-2">{title}</h5>
+			<div className=" flex mb-2">{stars}</div>
+            <div className=" flex flex-col-2 justify-between">
+                <p>{price}</p>
+                <img src={heartLogo} alt="" className=' w-9 h-8'/>
+            </div>
+		</div>
+	);
+};
+
+
+export default CustomerServicesCard
